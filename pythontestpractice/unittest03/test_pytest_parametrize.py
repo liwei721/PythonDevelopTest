503ec8052239c3188ed133790681196258357e55
zhouliwei <zhoulw@114995.com>
2020-06-18 22:31:14 +0800

    更新代码

--- pythontestpractice/unittest03/test_pytest_parametrize.py
+++ pythontestpractice/unittest03/test_pytest_parametrize.py
@@ -16,7 +16,7 @@ def test_login(name, pwd):
 
 # 第2种情况，直接在class上添加参数化装饰器
 @pytest.mark.parametrize(("name", "pwd"), name_pwd_list)
-class TestCase():
+class TestCase:
 
     def test_login_01(self, name, pwd):
         print(f"login with name={name},pwd={pwd}")
@@ -25,12 +25,37 @@ class TestCase():
         print(f"login with name={name},pwd={pwd}")
 
 
-# 第三种情况，在函数上有多个参数化装饰器。
-data_1 = [1, 2, 3]
-data_2 = ['a', 'b']
+# 第3中情况通过ids修改用例名称
+ids = ["{} login system use pwd {}".format(name, pwd) for name, pwd in name_pwd_list]
 
 
-@pytest.mark.parametrize('a', data_1)
-@pytest.mark.parametrize('b', data_2)
-def test_many_fixture(a, b):
-    pass
+@pytest.mark.parametrize(("name", "pwd"), name_pwd_list, ids=ids)
+def test_ids(name, pwd):
+    print("name is {}, pwd is {}".format(name, pwd))
+
+
+# # 第4种情况，在函数上有多个参数化装饰器。
+do_list = ["swim", "play", "eat"]
+name_list = ['zhangsan', 'lisi']
+
+
+@pytest.mark.parametrize('do', do_list)
+@pytest.mark.parametrize('name', name_list)
+def test_many_fixture(name, do):
+    print(f"{name} has do {do}")
+
+
+# 第5种情况，indirect=True
+user_data = [('lisi', '23'), ('zhangsan', '25'), ('wangwu', '27')]
+
+
+@pytest.fixture()
+def user_info(request):
+    param = request.param
+    print("{} is {} year old".format(param[0], param[1]))
+    return param
+
+
+@pytest.mark.parametrize("user_info", user_data, indirect=True)
+def test_get_user_info(user_info):
+    print("I'm {}, {} years old".format(user_info[0], user_info[1]))
