2c324144dd4383bfc0df9a6674e6d4eee1e01283
xuanke <zhouliwei1989@126.com>
2020-06-18 14:03:56 +0800

    更新代码

--- pythontestpractice/unittest03/test_pytest_parametrize.py
+++ pythontestpractice/unittest03/test_pytest_parametrize.py
@@ -5,7 +5,32 @@
 """
 import pytest
 
+name_pwd_list = [("zs", "123456"), ("ls", "123456")]
 
-@pytest.mark.parametrize(("name", "pwd"), [("zs", "123456"), ("ls", "123456")])
+
+# 第一种情况，直接在函数上添加参数化装饰器
+@pytest.mark.parametrize(("name", "pwd"), name_pwd_list)
 def test_login(name, pwd):
     print(f"{name}====={pwd}")
+
+
+# 第2种情况，直接在class上添加参数化装饰器
+@pytest.mark.parametrize(("name", "pwd"), name_pwd_list)
+class TestCase():
+
+    def test_login_01(self, name, pwd):
+        print(f"login with name={name},pwd={pwd}")
+
+    def test_login_02(self, name, pwd):
+        print(f"login with name={name},pwd={pwd}")
+
+
+# 第三种情况，在函数上有多个参数化装饰器。
+data_1 = [1, 2, 3]
+data_2 = ['a', 'b']
+
+
+@pytest.mark.parametrize('a', data_1)
+@pytest.mark.parametrize('b', data_2)
+def test_many_fixture(a, b):
+    pass
