9a664fb18a2e7a8e18d04312e77a0b8c8ad1f270
zhouliwei <zhoulw@114995.com>
2020-08-07 06:51:03 +0800

    更新文章

--- pythontestpractice/appium_test07/find_element.py
+++ pythontestpractice/appium_test07/find_element.py
@@ -22,5 +22,32 @@ def find_element_by_id():
     btn_login.click()
 
 
+def find_element_by_name():
+    desired_caps = {'platformName': 'Android', 'platformVersion': '8.0', 'deviceName': 'Android Emulator',
+                    'appPackage': 'tv.danmaku.bili', 'appActivity': 'tv.danmaku.bili.MainActivityV2'}
+    # 连接appium server，告诉appium，代码要操作哪个设备上的哪个APP
+    driver = webdriver.Remote('http://127.0.0.1:4723/wd/hub', desired_caps)
+    # 添加时间间隔,暂时先使用time.sleep线程中断
+    time.sleep(15)
+
+    # 登录按钮
+    btn_login = driver.find_element_by_name("登录")
+    btn_login.click()
+
+
+def find_element_by_android_uiautomator():
+    desired_caps = {'platformName': 'Android', 'platformVersion': '8.0', 'deviceName': 'Android Emulator',
+                    'appPackage': 'tv.danmaku.bili', 'appActivity': 'tv.danmaku.bili.MainActivityV2'}
+    # 连接appium server，告诉appium，代码要操作哪个设备上的哪个APP
+    driver = webdriver.Remote('http://127.0.0.1:4723/wd/hub', desired_caps)
+    # 获取用户隐私协议弹窗
+    agree_btn = driver.find_element_by_id("tv.danmaku.bili:id/agree")
+    agree_btn.click()
+    # 添加时间间隔,暂时先使用time.sleep线程中断
+    time.sleep(20)
+    btn_login = driver.find_element_by_android_uiautomator("new UiSelector().text(\"登录\")")
+    btn_login.click()
+
+
 if __name__ == '__main__':
-    find_element_by_id()
+    find_element_by_android_uiautomator()
